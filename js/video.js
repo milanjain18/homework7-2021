225b219135ec3d0faf666ea255162e001f8c0b87
milanjain18 <55509711+milanjain18@users.noreply.github.com>
2021-04-02 22:39:32 -0400

    Update video.js

--- js/video.js
+++ js/video.js
@@ -1,13 +1,13 @@
-// var video;
+var video;
 
-// window.addEventListener("load", function() {
-// 	console.log("Good job opening the window")
+window.addEventListener("load", function() {
+	console.log("Good job opening the window")
 
+});
+
+// document.querySelector("#play").addEventListener("click", function() {
+// 	console.log("Play Video");
 // });
-
-// // document.querySelector("#play").addEventListener("click", function() {
-// // 	console.log("Play Video");
-// // });
 
 
 document.querySelector("#slower").addEventListener("click", function() {
