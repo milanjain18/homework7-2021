a455f6b5d4482964e3034c08034c1ccb46d5f3a4
milanjain18 <55509711+milanjain18@users.noreply.github.com>
2021-04-03 19:20:15 -0400

    Update video.js

--- js/video.js
+++ js/video.js
@@ -1,84 +1,82 @@
 var video = document.getElementById("player1");
 console.log("Hello");
 
-window.addEventListener("load", function() {
-	video = document.querySelector(#player1);
+window.addEventListener("load", function() 
+{
 	console.log("Good job opening the window")
 
 });
 
-document.querySelector("#play").addEventListener("click", function() {
+document.querySelector("#play").addEventListener("click", function() 
+{
 	console.log("Play Video");
-	let vol = document.querySelector(#volume).innerHTML = video.volume * 100 + '%';
-	//check with gsi if you can declare with let like that?????
+	video.play();
+	let vol = document.querySelector("#volume").innerHTML = video.volume * 100 + ' %';
+});
+
+document.querySelector("#pause").addEventListener("click", function() 
+{
+	console.log("Pause Video");
+	video.pause();
 });
 
 
-document.querySelector('#orig').addEventListener('click', function(){
+document.querySelector("#vintage").addEventListener("click", function() 
+{
+	console.log('Vintage');
+	video.className = "oldSchool";
+});
+
+document.querySelector("#orig").addEventListener("click", function() 
+{
 	console.log("Original");
-	video.classList.remove("oldSchool");
-
+	video.className = "video";
 });
 
-document.querySelector('#vintage').addEventListener('click', function(){
-	console.log("Vintage");
-	video.classList.remove("oldSchool");
-
+//are these the only changes from slower to faster methods?? ask GSI
+document.querySelector("#slower").addEventListener("click", function() 
+{
+	video.playbackRate *= 0.95;
+	console.log("Speed is " + video.playbackRate);
 });
-	
 
-	
-document.querySelector("#mute").addEventListener("click", function() {
-	if (video.muted == false)
+document.querySelector("#faster").addEventListener("click", function() 
+{
+	video.playbackRate *= 1.05;
+	console.log("Speed is " + video.playbackRate);
+});
+
+document.querySelector("#skip").addEventListener("click", function() 
+{
+	if (video.currentTime + 15 >= video.duration)
 	{
-		video.muted = true;
-		document.querySelector("#mute").innerHTML = "Unmute";
+		video.currentTime = 0;
 	} 
 	else 
 	{
+		video.currentTime = video.currentTime + 15;
+		console.log(video.currentTime);
+	}
+});
+
+document.querySelector('#slider').addEventListener('change', function() 
+{
+	console.log(this.value / 100);
+	video.volume = this.value / 100;
+	let vol = document.querySelector("#volume").innerHTML = video.volume * 100 + '%';
+});
+
+
+document.querySelector("#mute").addEventListener("click", function() 
+{
+	if (video.muted == false){
+		video.muted = true;
+		document.querySelector("#mute").innerHTML = "Unmute";
+	} else {
 		video.muted = false;
 		document.querySelector("#mute").innerHTML = "Mute";
 	}
 });
-
-document.querySelector("#slider").addEventListener ("change", function()
-{
-	console.log(this.value);
-	var slider = this.value;
-	video.volume = slider / 100;
-	document.querySelector("#volume").innerHTML = video.volume*100+ '%'
-});
-
-document.querySelector("#slower").addEventListener("click", function() {
-	video.playbackRate *= 0.95;
-	console.log(video.playbackRate);
-});
-
-
-//for faster, just multiple vs divide, does this not compound/regress
-
-document.querySelector("#faster").addEventListener("click", function() {
-	video.playbackRate *= 1.95;
-	console.log(video.playbackRate);
-});
-
-
-
-document.querySelector("#skip").addEventListener("click", function() {
-	if (video.currentTime + 15 < video.duration)
-	{
-		console.log(video.currentTime);
-		video.currentTime += 15;
-	} 
-	else 
-	{
-		video.currentTime = 0;
-	}
-});
-
-	
-	
-
 
 
 
